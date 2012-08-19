41b1345c3b026d273d7e533707694f11996415b6
David Van Der Beek <earlynovrock@gmail.com>
2012-08-19 17:27:29 -0500

    add dashboard layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,16 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
 	 dashboard_path
 	end
+
+	layout :layout_by_resource
+
+  protected
+
+  def layout_by_resource
+    if devise_controller? && resource_name == :user && action_name == 'edit'
+      "dashboard"
+    else
+      "application"
+    end
+  end
 end
