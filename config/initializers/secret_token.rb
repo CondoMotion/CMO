aeb8940044787a36d7380b1b6dbfe8e90c2e6b37
David Van Der Beek <earlynovrock@gmail.com>
2012-08-19 11:16:39 -0500

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Condomotion::Application.config.secret_token = 'f8350fefb2f1b912bdea4711f280bfae69f3d96b1030a02329ad45e0c4d4946d24e5113d3284bd85a996d03948dbb1101e8eb42f5c81a3f0ece2c6377e255bcb'
